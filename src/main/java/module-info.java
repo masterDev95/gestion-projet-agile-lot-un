4cc9b92900a67016d9165325a9c79d5562a86f8e
masterDev95 <masdevelopper@gmail.com>
2023-06-09 15:29:42 +0200

    Connection à la BDD (local)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,10 +2,12 @@ module com.lotun.gestionprojetagilelotun {
     requires javafx.controls;
     requires javafx.fxml;
     requires jakarta.xml.bind;
+    requires java.sql;
 
     opens com.lotun.gestionprojetagilelotun to javafx.fxml;
     opens com.lotun.gestionprojetagilelotun.controllers to javafx.fxml;
     opens com.lotun.gestionprojetagilelotun.classes to jakarta.xml.bind, javafx.base;
+    opens com.lotun.gestionprojetagilelotun.dao to java.sql;
     exports com.lotun.gestionprojetagilelotun;
     exports com.lotun.gestionprojetagilelotun.controllers;
 }
