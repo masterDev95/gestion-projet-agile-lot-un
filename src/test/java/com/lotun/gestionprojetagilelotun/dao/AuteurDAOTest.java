1bffc4a3af6639a89934674f8016d3923872cafa
Kesu19 <74417754+Kesu19@users.noreply.github.com>
2023-06-15 15:56:29 +0200

    add test unitaire

---
+++ src/test/java/com/lotun/gestionprojetagilelotun/dao/AuteurDAOTest.java
@@ -0,0 +1,45 @@
+package com.lotun.gestionprojetagilelotun.dao;
+
+import com.lotun.gestionprojetagilelotun.classes.Auteur;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuteurDAOTest {
+
+    @Test
+    void getAuteurFromDBById() throws SQLException {
+        Auteur auteur = AuteurDAO.getAuteurFromDBById(2);
+        Auteur auteurAttendu = new Auteur();
+        auteurAttendu.setNom("SUDROT");
+        auteurAttendu.setPrenom("KELVIN");
+        assertTrue(auteurAttendu.getNom().equals(auteur.getNom()) && auteurAttendu.getPrenom().equals(auteur.getPrenom()));
+    }
+
+    @Test
+    void testInsertAuteur() throws SQLException {
+        // Créer une connexion à une base de données (pour les besoins du test)
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bibliothèque", "root", "root");
+
+        // Créer un objet Auteur pour le test
+        Auteur auteur = new Auteur();
+        auteur.setNom("moi");
+        auteur.setPrenom("moi");
+        // Appeler la fonction à tester
+        int auteurId = AuteurDAO.insertAuteur(auteur, connection);
+
+        // Vérifier le résultat attendu
+        assertTrue(auteurId > 0);
+
+        // Fermer la connexion à la base de données (après le test)
+        connection.close();
+    }
+
+    @Test
+    void getAuteurId() {
+    }
+}
